8debcd3d58dbd0890d9a884ac113898b5cf87426
Gilson Vicente <proitandroit@gmail.com>
2019-04-09 15:45:12 +0100

    LoginFacebook_ADDed

--- app/src/main/java/proitappsolutions/com/rumosstore/MediaRumoActivity.java
+++ app/src/main/java/proitappsolutions/com/rumosstore/MediaRumoActivity.java
@@ -1,12 +1,38 @@
 package proitappsolutions.com.rumosstore;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.wang.avi.AVLoadingIndicatorView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import proitappsolutions.com.rumosstore.telasIniciais.HomeInicialActivity;
 
@@ -16,13 +42,69 @@ public class MediaRumoActivity extends AppCompatActivity implements View.OnClick
     private ImageView btnLogFb;
     private Button btnEntrar,btnRegistrate;
 
+    LoginButton loginButton;
+    AVLoadingIndicatorView loader;
+    CallbackManager callbackManager;
+    AccessToken accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_media_rumo);
         //comit
         inicializar();
 
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("email","public_profile"));
+
+        btnLogFb.setEnabled(true);
+        btnLogFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginButton.performClick();
+                btnLogFb.setEnabled(false);
+//                loader.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                accessToken = loginResult.getAccessToken();
+
+                btnLogFb.setEnabled(false);
+                loader.setVisibility(View.VISIBLE);
+
+                if (!isConnected(10000)) {
+                    loader.setVisibility(View.INVISIBLE);
+                    LoginManager.getInstance().logOut();
+
+                    btnLogFb.setEnabled(true);
+
+                } else{
+                    loaduserProfile(accessToken);
+                }
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                loader.setVisibility(View.INVISIBLE);
+                btnLogFb.setEnabled(true);
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                loader.setVisibility(View.INVISIBLE);
+                btnLogFb.setEnabled(true);
+
+            }
+        });
     }
 
     private void inicializar() {
@@ -33,7 +115,10 @@ public class MediaRumoActivity extends AppCompatActivity implements View.OnClick
         btnRegistrate = findViewById(R.id.btnRegistrate);
         btnEntrar.setOnClickListener(MediaRumoActivity.this);
         btnRegistrate.setOnClickListener(MediaRumoActivity.this);
-        btnLogFb.setOnClickListener(MediaRumoActivity.this);
+//        btnLogFb.setOnClickListener(MediaRumoActivity.this);
+
+        loginButton = findViewById(R.id.loginBtn);
+        loader = findViewById(R.id.loader);
     }
 
     @Override
@@ -51,9 +136,86 @@ public class MediaRumoActivity extends AppCompatActivity implements View.OnClick
                 startActivity(intentRegistrar);
                 break;
 
-            case R.id.btnLogFb:
-                break;
+//            case R.id.btnLogFb:
+//                break;
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void loaduserProfile(AccessToken newAccessToken){
+
+        loader.setVisibility(View.VISIBLE);
+
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+                    String name = first_name + " "+last_name;
+
+                    String image_url = "https://graph.facebook.com/"+id+ "/picture?type=normal";
+
+                    Usuario user = new Usuario(id,email,name,image_url);
+
+                    AppDatabase.saveUser(user);
+
+                    AppPref.getInstance().saveAuthToken(newAccessToken);
+                    Intent intentEntrar = new Intent(MediaRumoActivity.this,HomeInicialActivity.class);
+                    intentEntrar.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intentEntrar);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+    }
+
+    private void launchHomeScreen() {
+        Intent intent = new Intent(MediaRumoActivity.this, HomeInicialActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private boolean isConnected(int timeOut) {
+        InetAddress inetAddress = null;
+        try {
+            Future<InetAddress> future = Executors.newSingleThreadExecutor().submit(new Callable<InetAddress>() {
+                @Override
+                public InetAddress call() {
+                    try {
+                        return InetAddress.getByName("google.com");
+                    } catch (UnknownHostException e) {
+                        return null;
+                    }
+                }
+            });
+            inetAddress = future.get(timeOut, TimeUnit.MILLISECONDS);
+            future.cancel(true);
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e) {
+        } catch (TimeoutException e) {
+        }
+        return inetAddress != null && !inetAddress.equals("");
     }
 }
--- app/src/main/java/proitappsolutions/com/rumosstore/telasIniciais/HomeInicialActivity.java
+++ app/src/main/java/proitappsolutions/com/rumosstore/telasIniciais/HomeInicialActivity.java
@@ -1,6 +1,8 @@
 package proitappsolutions.com.rumosstore.telasIniciais;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,16 +11,36 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.facebook.login.LoginManager;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import proitappsolutions.com.rumosstore.AppDatabase;
+import proitappsolutions.com.rumosstore.AppPref;
+import proitappsolutions.com.rumosstore.Common;
+import proitappsolutions.com.rumosstore.MainActivity;
+import proitappsolutions.com.rumosstore.MediaRumoActivity;
 import proitappsolutions.com.rumosstore.R;
+import proitappsolutions.com.rumosstore.Usuario;
+import proitappsolutions.com.rumosstore.fragmentos.FragConcurso;
 import proitappsolutions.com.rumosstore.fragmentos.FragHomeInicial;
 import proitappsolutions.com.rumosstore.fragmentos.FragMediaRumo;
 import proitappsolutions.com.rumosstore.fragmentos.FragMercado;
 import proitappsolutions.com.rumosstore.fragmentos.FragVanguarda;
 
 public class HomeInicialActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    CircleImageView circleImageView;
+    TextView txtName;
+    TextView txtEmail;
+
+    Usuario usuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +49,21 @@ public class HomeInicialActivity extends AppCompatActivity implements Navigation
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("");
+        toolbar.setTitle("Rumos Store");
         setSupportActionBar(toolbar);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setItemIconTintList(null);
         View headerView = navigationView.getHeaderView(0);
+
+        circleImageView = (CircleImageView) headerView.findViewById(R.id.iv_imagem_perfil);
+        txtName = (TextView) headerView.findViewById(R.id.txtName);
+        txtEmail = (TextView) headerView.findViewById(R.id.txtEmail);
+
+        usuario = AppDatabase.getUser();
+
+        loaduserProfile(usuario);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -61,6 +91,48 @@ public class HomeInicialActivity extends AppCompatActivity implements Navigation
 
     }
 
+    private void loaduserProfile(Usuario usuario){
+
+        if (usuario !=null){
+            Picasso.with(HomeInicialActivity.this)
+                    .load(usuario.getUsuarioPic())
+                    .placeholder(R.drawable.ic_avatar)
+                    .into(circleImageView);
+
+
+            txtName.setText(usuario.getUsuarioNome());
+            txtEmail.setText(usuario.getUsuarioEmail());
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.rumos_store, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            logOut();
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int id = menuItem.getItemId();
@@ -73,7 +145,19 @@ public class HomeInicialActivity extends AppCompatActivity implements Navigation
             fragmentTransaction.replace(R.id.container,fragHomeInicial);
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
-        } else if (id == R.id.nav_mercado) {
+        } else if (id == R.id.nav_concurso) {
+
+            FragConcurso fragConcurso = new FragConcurso();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.container,fragConcurso);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+
+        }
+
+
+        else if (id == R.id.nav_mercado) {
 
             FragMercado fragMercado = new FragMercado();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -101,7 +185,7 @@ public class HomeInicialActivity extends AppCompatActivity implements Navigation
             fragmentTransaction.commit();
 
         }else if (id == R.id.nav_instagram) {
-
+            openInstagram(Common.SOCIAL_INSTAGRAM);
 
 
         }  else if (id == R.id.nav_facebook) {
@@ -112,5 +196,28 @@ public class HomeInicialActivity extends AppCompatActivity implements Navigation
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void logOut(){
+        LoginManager.getInstance().logOut();
+        AppDatabase.clearData();
+        AppPref.getInstance().clearData();
+
+        Intent intent = new Intent(HomeInicialActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    protected void openInstagram(String username) {
+        Uri uri = Uri.parse("http://instagram.com/_u/" + username);
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+        likeIng.setPackage("com.instagram.android");
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://instagram.com/" + username)));
+        }
     }
 }
