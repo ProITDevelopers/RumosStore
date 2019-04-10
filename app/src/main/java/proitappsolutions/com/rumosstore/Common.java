0aaf853aa4b4145cd01efe0e52c61eb8ceddede3
Gilson Vicente <proitandroit@gmail.com>
2019-04-10 09:07:16 +0100

    LoginFacebook_ADDed

---
+++ app/src/main/java/proitappsolutions/com/rumosstore/Common.java
@@ -0,0 +1,21 @@
+package proitappsolutions.com.rumosstore;
+
+public class Common {
+
+    public static final int GALLERY_PICK = 1;
+    public static String APP_CATEGORY = "News";
+
+    public static final String PAYPAL_CLIENT_ID = "AcWb_OrCdYDpYwI0UL_jQ3Az-dj0akEzIxFLj8yVWCDP9gW8d36a38sfyPHhDC4DNZjZ5ORtzj6rXPUh";
+    public static final int PAYPAL_REQUEST_CODE = 123;
+    public static Usuario mCurrentUser;
+
+    public static final String DB_REALM = "rumos_store_db";
+    public static final String ORDER_STATUS_COMPLETED = "approved";
+
+    //Your social Page ID or Username here. Not links....
+    public static String SOCIAL_FACEBOOK = "Media-Rumo";
+    public static String SOCIAL_LINKEDIN = "media-rumo";
+    public static String SOCIAL_INSTAGRAM = "revistarumo";
+    //Your website homepage link in below String item....
+    public static String SOCIAL_BROWSER = "https://mediarumo.com/";
+}
---
+++ app/src/main/java/proitappsolutions/com/rumosstore/Usuario.java
@@ -0,0 +1,58 @@
+package proitappsolutions.com.rumosstore;
+
+import com.google.gson.annotations.SerializedName;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Usuario extends RealmObject {
+
+    @PrimaryKey
+    private String usuarioId;
+    private String usuarioEmail;
+    private String usuarioNome;
+    @SerializedName("usuarioPic")
+    private String usuarioPic;
+    public String usuarioToken;
+
+    public Usuario() {}
+
+    public Usuario(String usuarioId, String usuarioEmail, String usuarioNome, String usuarioPic) {
+        this.usuarioId = usuarioId;
+        this.usuarioEmail = usuarioEmail;
+        this.usuarioNome = usuarioNome;
+        this.usuarioPic = usuarioPic;
+    }
+
+    public String getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(String usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getUsuarioEmail() {
+        return usuarioEmail;
+    }
+
+    public void setUsuarioEmail(String usuarioEmail) {
+        this.usuarioEmail = usuarioEmail;
+    }
+
+    public String getUsuarioNome() {
+        return usuarioNome;
+    }
+
+    public void setUsuarioNome(String usuarioNome) {
+        this.usuarioNome = usuarioNome;
+    }
+
+    public String getUsuarioPic() {
+        return usuarioPic;
+    }
+
+    public void setUsuarioPic(String usuarioPic) {
+        this.usuarioPic = usuarioPic;
+    }
+}
